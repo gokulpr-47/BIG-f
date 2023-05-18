f6751e22b27b2e39ffbcecca64fa9a5efc0e09fd
Rafi <itsrealrafi@gmail.com>
2023-05-19 00:09:41 +0530

    Landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,9 +1,80 @@
 import React from 'react'
-
+import './LandingPage.css'
 function LandingPage() {
   return (
-    <div>
-        Landing PAge
+    <div className='landingpage'>
+      <div className="lpheader">
+        <div className="lplogodiv">
+          <img src="\images\logo.svg" alt="" className="lplogo" />
+        </div>
+        <div className="lp3cdiv">
+          <img src="\images\Ellipse_stroke.svg" alt="" className="lp3c" />
+          <img src="\images\Ellipse_stroke.svg" alt="" className="lp3c" />
+          <img src="\images\Ellipse_stroke.svg" alt="" className="lp3c" />
+        </div>
+        <div className="lpyellowdiv">
+          <img src="\images\yellow.svg" alt="" className="oneyellow" />
+        </div>
+
+      </div>
+      <div className="lpdowndiv">
+        <div className="lpdownleft">
+          <div className="orangediv">
+            <div className="divelem3">
+              <div className="elem1">
+                <img src="\images\yellow.svg" alt="" className="yellowc" />
+              </div>
+              <div className="elem2">
+                <img src="\images\Ellipse_fill.svg" alt="" className="dots" />
+                <img src="\images\Ellipse_fill.svg" alt="" className="dots" />
+                <img src="\images\Ellipse_fill.svg" alt="" className="dots" />
+              </div>
+              <div className="divline">
+                <img src="\images\beveledline.svg" alt="" className="line" />
+              </div>
+            </div>
+            <div className="heading">
+              <h1>BIG-f<strong>.</strong> </h1>
+            </div>
+            <div className="abtus">
+              <img src="\images\Component 2.svg" alt="" className="abtsusimg" />
+            </div>
+
+
+            <div className="allcontainer">
+
+              <div className="arrow">
+                <img src="\images\Component 3.svg" alt="" className="arrowsign" />
+              </div>
+              <div className="lpbtn btn">
+                <button className=" butonbigger big-button">signup</button>
+                <button className=" butonbigger big-button">Login</button>
+              </div>
+
+            </div>
+
+
+
+          </div>
+        </div>
+        <div className="lpdownright">
+          <div className="smollline">
+            <img src="\images\Line 42.svg" alt="" className="line42" />
+          </div>
+
+
+
+          <div className="complex">
+            <img src="\images\grid.svg" alt="" className="grid" />
+          </div>
+          <div className="imgss">
+            <img src="\images\card.svg" alt="" className="img1" />
+            <img src="\images\card.svg" alt="" className="img2" />
+            <img src="\images\card.svg" alt="" className="img3" />
+            <img src="\images\card.svg" alt="" className="img4" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
