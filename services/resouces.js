22c964e0f6e60f841b2fd7e6f238e2b09048ad5a
mohanthilak <mohan.thilak410@gmail.com>
2023-05-18 20:54:39 +0530

    curation done

--- services/resouces.js
+++ services/resouces.js
@@ -2,6 +2,12 @@ const ErrorMessage = "Error in Resources Service Layer:";
 const axios = require('axios');
 const {OPENAI_SECRET_KEY} = require("../config/index")
 
+const replaceExcapeSequence =(text) =>{
+    console.log(text)
+    text = text.replace(/[\r\n]/gm, '');
+    return text;
+}
+
 class ResourcesService{
     resourceRepo;
     openAi = new OpenAI();
@@ -11,12 +17,22 @@ class ResourcesService{
     }
     async createRources ({author, inputNotes, topic, deadline, studyType, visibilty}){
         try{
+            let response = ""
             //to get the topics for the 
-            const topics = this.openAi.getTopicsFromText(inputNotes);
-            // const prompt = "elaborate the following topics '" + topics +"'";
-            // const curatedNotes  = this.openAi.textCompletion(prompt);
-            // this.resourceRepo.cre
-            return {success:true, data:topics};
+            let topics = await this.openAi.getTopicsFromText(inputNotes);
+            console.log(topics, "!!!!!!!!!!", typeof topics);
+            if(topics){
+                response = topics;
+                topics = replaceExcapeSequence(topics);
+                console.log(topics);
+                const prompt = "elaborate the following topics mentioned in the array " + topics;
+                const curatedNotes  = await this.openAi.textCompletion(prompt);
+                console.log(curatedNotes, "line 28");
+                response = curatedNotes
+            }else{
+                console.log("It ain't string")
+            }
+            return {success:true, data:response};
         }catch(e){
             console.log(ErrorMessage, e);
             return {success: false, error: e}
@@ -37,42 +53,57 @@ class OpenAI {
                 'Authorization': `Bearer ${OPENAI_SECRET_KEY}`
             }
         })
-        // text = "what are the main topics covered in the following text " + text + " the output must contain only the topic names"
+        text = replaceExcapeSequence(text);
+        console.log(text)
+        text = "what are the main topics covered in the following text " + text + " the output must contain only the topic names in an array. ouput should not contain any excape sequence"
         try{
-            axiosPrivate.post("/v1/completions", {
+            let response = await axiosPrivate.post("/v1/completions", {
                 "model": "text-davinci-003",
-                "prompt": "Say this is a test",
-                "max_tokens": 7,
+                "prompt": text,
+                "max_tokens": 1700,
                 "temperature": 0,
                 "top_p": 1,
                 "n": 1,
                 "stream": false,
                 "logprobs": null,
-              }).then(res=>{
-                console.log(res);
-              }).catch(e=>{
-                console.log("Error while contacting openAI", e)
               })
-            return axiosPrivate
+            return response.data.choices[0].text
         }catch(e){
             console.log("Error while contact openAI for text to topic", e);
         }
     }
 
     async textCompletion(prompt) {
-        this.privateAxios.post("/v1/completions", {
-          "model": "text-davinci-003",
-          "prompt": prompt,
-          "max_tokens": 500,
-          "temperature": 0,
-          "top_p": 1,
-          "n": 1,
-          "stream": false,
-          "logprobs": null
-        }).then(res=>{
-            console.log(res);
-            return res.choices[0].text;
-        })
+        try {
+
+            const axiosPrivate = axios.create({
+                baseURL: 'https://api.openai.com',
+                headers: {
+                    "Content-Type": "application/json",
+                    'Authorization': `Bearer ${OPENAI_SECRET_KEY}`
+                }
+            })
+            
+            const response = await axiosPrivate.post("/v1/completions", {
+                "model": "text-davinci-003",
+                "prompt": prompt,
+                "max_tokens": 500,
+                "temperature": 0,
+                "top_p": 1,
+                "n": 1,
+                "stream": false,
+                "logprobs": null
+            }).then(res=>{
+                console.log(res?.data.choices[0].text, "line 94")
+                return res?.data.choices[0].text
+            })
+            console.log(response, "line 96");
+            return response;
+        }catch(e){
+            console.log("Error while curation", e);
+            return null
+        }
+
     }
 }
 
