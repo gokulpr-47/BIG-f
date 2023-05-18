218253ede5b36a90300c58b1e741568d81d520bc
Rafi <itsrealrafi@gmail.com>
2023-05-19 03:06:22 +0530

    abOUT US PAGE

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Login from './components/Login/Login'
 import SignUp from './components/SignUp/SignUp'
 import UltimateTrash from './components/trash/UltimateTrash'
 import LandingPage from './components/LandingPage/LandingPage'
-
+import AboutUs from './components/Aboutus/AboutUs'
 function App() {
 
   return (
@@ -11,7 +11,8 @@ function App() {
      {/* <Login /> */}
      {/* <SignUp /> */}
      {/* <UltimateTrash /> */}
-     <LandingPage />
+     {/* <LandingPage /> */}
+     <AboutUs />
     </>
   )
 }
---
+++ src/components/Aboutus/AboutUs.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './AboutUs.css'
+function AboutUs() {
+  return (
+    <div className='abtcontainer'>
+      <div className="descriptionabt">
+        <div className="imageabt">
+        </div>
+        <div className="textabt">
+          <div className="abtfirst">
+            About BIG-f
+          </div>
+          <div className="abtsecond">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis sequi aliquid aperiam placeat id, sapiente in veniam aspernatur iure ducimus dolorem cum eaque quibusdam ut. Aspernatur a rerum, vel aliquam optio inventore quam officiis cumque.
+            </p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default AboutUs
