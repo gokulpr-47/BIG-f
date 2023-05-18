cb0c1cac08340d360645067f66edd98fec57443e
Rafi <itsrealrafi@gmail.com>
2023-05-18 12:35:01 +0530

    Header contents added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css'
-import LandingPage from './components/LandingPage/LandingPage'
-
+import Login from './components/Login/Login'
 function App() {
 
   return (
     <>
-      <LandingPage/>
+     <Login />
     </>
   )
 }
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Login.css'
+function Login() {
+  return (
+    <div className="login">
+        <div className="up">
+            <div className="left">
+                <img src="\images\logo.svg" alt="" className="logo" />
+            </div>
+            <div className="right">
+                <div className="big-button">
+                    <button>signup</button>
+                </div>
+                <div className="symbol">
+                    <img src="\images\3circles.svg" alt="" className="circles" />
+                </div>
+            </div>
+        </div>
+        <div className="down">
+down
+        </div>
+    </div>
+  )
+}
+
+export default Login
